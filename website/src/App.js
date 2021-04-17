2c28ef954cb0e36512bd2c4ff86ef3fd61dac8ec
Simon <simonfong6@gmail.com>
2021-04-17 13:41:26 -0700

    Clean up deployment flow

--- website/src/App.js
+++ website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 1:36
+          Edit <code>src/App.js</code> and save to reload. 1:38
         </p>
         <a
           className="App-link"
